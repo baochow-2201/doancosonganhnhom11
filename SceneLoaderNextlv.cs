e3cafd08080a503e446b137948568db399956180
BChow-2201 <2331540106@vaa.edu.vn>
2025-03-31 13:17:29 +0700

    Add files via upload

---
+++ SceneLoaderNextlv.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoaderNextlv : MonoBehaviour
+{
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene("MainGame2");  
+    }
+}
